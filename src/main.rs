a08f74acff9b9ddaaf32555f10ce43bcf3ebf9b5
Nana Akwah <fludlyt@users.noreply.github.com>
2022-10-07 22:35:37 +0100

    Replace panic, fix spelling mistake

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
 
-/// Search fpt a pattern in a file and display the lines that contain it.
+/// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
 struct Cli {
 	/// The pattern to look for
@@ -9,16 +9,17 @@ struct Cli {
 	path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
 	let args = Cli::parse();
 
 	let result = std::fs::read_to_string(&args.path);
 	let content = match result {
 		Ok(content) => content,
 		Err(error) => {
-			panic!("Can't deal with {}, just exit here", error);
+			return Err(error.into());
 		}
 	};
 
 	println!("file content: {}", content);
+	Ok(())
 }
