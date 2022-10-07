f93387e4b488cc17bd7126fdce51a488fbb3178d
Nana Akwah <fludlyt@users.noreply.github.com>
2022-10-07 21:57:41 +0100

    Parse args using clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use clap::Parser;
+
+/// Search fpt a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+	/// The pattern to look for
+	pattern: String,
+	/// The path to the file to read
+	path: std::path::PathBuf,
+}
+
 fn main() {
-	println!("Hello, world!");
+	let args = Cli::parse();
 }
