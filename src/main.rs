8088f9b0395242a69ebef12748f48160f1a72e08
Nana Akwah <fludlyt@users.noreply.github.com>
2022-10-07 22:31:09 +0100

    Match on result

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,13 @@ struct Cli {
 fn main() {
 	let args = Cli::parse();
 
-	let content = std::fs::read_to_string(&args.path).expect("could not read file");
-
-	for line in content.lines() {
-		if line.contains(&args.pattern) {
-			println!("{}", line);
+	let result = std::fs::read_to_string(&args.path);
+	let content = match result {
+		Ok(content) => content,
+		Err(error) => {
+			panic!("Can't deal with {}, just exit here", error);
 		}
-	}
+	};
+
+	println!("file content: {}", content);
 }
